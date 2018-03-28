4aaf59f0c1b2202a2c765c0c295ceaa2090a04ad
Matt Farmer <mrfarmer777@gmail.com>
2018-03-28 04:14:40 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,13 @@
 def fizzbuzz(int)
   if int % 3 == 0
-    "Fizz"
+    if int % 5==0
+      "FizzBuzz"
+    else
+      "Fizz"
+    end
   elsif int % 5 == 0
     "Buzz"
-  elsif (int % 3 == 0 && int % 5 == 0)
-    "FizzBuzz"
+  
   else
     nil
   end
